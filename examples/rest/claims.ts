5914e2711968389ee24a1bd511badb9fcd9c3692
Hanchon <guillermo.paoletti@gmail.com>
2022-01-31 09:26:43 +0100

    code clean up

--- examples/rest/claims.ts
+++ examples/rest/claims.ts
@@ -4,46 +4,46 @@ import { getTotalUnclaimed, getClaimsRecord } from '../../src/rest/claims'
     let client = new RestInstance('http://rest.evmos.me/')
     await getTotalUnclaimed(client)
     /*
-        {
-            coins:
-            [
-                { denom: 'aevmos', amount: '7500' }
-            ],
-            error: ''
-        }
-        */
+            {
+                coins:
+                [
+                    { denom: 'aevmos', amount: '7500' }
+                ],
+                error: ''
+            }
+            */
 
-    let a = await getClaimsRecord(
+    await getClaimsRecord(
         client,
         'evmos1qc3xt7vctgwesrlfytve5yja8qcja4tcjkxemy'
     )
     /*
-        {
-            initial_claimable_amount: '10000',
-            claims: [
-                {
-                action: 'ACTION_VOTE',
-                claimable_amount: '1236',
-                completed: false
-                },
-                {
-                action: 'ACTION_DELEGATE',
-                claimable_amount: '0',
-                completed: true
-                },
-                {
-                action: 'ACTION_EVM',
-                claimable_amount: '1236',
-                completed: false
-                },
-                {
-                action: 'ACTION_IBC_TRANSFER',
-                claimable_amount: '1236',
-                completed: false
-                }
-            ],
-            address: 'evmos1gpapf6d8v85vwsl4w36n8l5qlchhp2j2drdyxg',
-            error: ''
-        }
-        */
+            {
+                initial_claimable_amount: '10000',
+                claims: [
+                    {
+                    action: 'ACTION_VOTE',
+                    claimable_amount: '1236',
+                    completed: false
+                    },
+                    {
+                    action: 'ACTION_DELEGATE',
+                    claimable_amount: '0',
+                    completed: true
+                    },
+                    {
+                    action: 'ACTION_EVM',
+                    claimable_amount: '1236',
+                    completed: false
+                    },
+                    {
+                    action: 'ACTION_IBC_TRANSFER',
+                    claimable_amount: '1236',
+                    completed: false
+                    }
+                ],
+                address: 'evmos1gpapf6d8v85vwsl4w36n8l5qlchhp2j2drdyxg',
+                error: ''
+            }
+            */
 })()
